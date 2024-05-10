96c42d30e0c27ed20885acd597728e0287d05dcb
Khrunev Denis <deniskhrunev@gmail.com>
2024-05-10 14:11:01 +0600

    Задание 1

---
+++ task1.sh
@@ -0,0 +1,5 @@
+mkdir -p project/my_project/directory1/subdirectory1/
+mkdir -p project/my_project/directory2/
+mkdir -p project/my_project/directory3/subdirectory3/
+cd project/my_project/directory2
+echo "Hello from JS" > hello.txt
